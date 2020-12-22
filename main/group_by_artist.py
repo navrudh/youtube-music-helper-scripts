7bae74d698e15e11bac427e42bd0a21e08163f88
Dhruvan Ganesh <navrudh@gmail.com>
2020-12-22 21:47:17 +0100

    fix string comparision

--- main/group_by_artist.py
+++ main/group_by_artist.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     playlist = get_user_playlist_by_title(yt_music, playlist_name, limit=100)
     if not playlist:
         playlist = create_playlist(yt_music, title=playlist_name)
-    tracks = search_uploaded_tracks(yt_music, artist_name, limit=50)
+    tracks = search_uploaded_tracks(yt_music, artist_name, limit=70)
     track_ids = [tr.id for tr in tracks]
     yt_music.add_playlist_items(playlist.id, track_ids)
